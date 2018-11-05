e07469ea5548a0a05a5c4ea84dd10c53b8112f82
AnneJTL <anne.chatin@jointheleague.org>
2018-11-05 11:24:57 -0800

    _03_TestMatchingBrackets

--- src/_03_IntroToStacks/_03_TestMatchingBrackets.java
+++ src/_03_IntroToStacks/_03_TestMatchingBrackets.java
@@ -2,29 +2,45 @@ package _03_IntroToStacks;
 
 import org.junit.jupiter.api.Test;
 
-/*
-import static org.junit.Assert.assertFalse;
-import static org.junit.Assert.assertTrue;
-
 import java.util.Stack;
 
-import org.junit.Test;
-*/
-public class _03_TestMatchingBrackets {
-/*
-	@Test
-	public void testMatchingBrackets() {
-		assertTrue(doBracketsMatch("{}"));
-		assertTrue(doBracketsMatch("{{}}"));
-		assertTrue(doBracketsMatch("{}{}{{}}"));
-		assertFalse(doBracketsMatch("{{}"));
-		assertFalse(doBracketsMatch("}{"));
-	}
+import static org.junit.jupiter.api.Assertions.*;
 
-	// USE A STACK TO COMPLETE THE METHOD FOR CHECKING IF EVERY OPENING BRACKET HAS A MATCHING CLOSING BRACKET
-	private boolean doBracketsMatch(String b) {
-		
-		return false;
-	}
-*/
+public class _03_TestMatchingBrackets {
+
+    @Test
+    public void testMatchingBrackets() {
+        assertTrue(doBracketsMatch("{}"));
+        assertTrue(doBracketsMatch("{{}}"));
+        assertTrue(doBracketsMatch("{}{}{{}}"));
+        assertFalse(doBracketsMatch("{{}"));
+        assertFalse(doBracketsMatch("}{"));
+    }
+
+    // USE A STACK TO COMPLETE THE METHOD
+    // FOR CHECKING IF EVERY OPENING BRACKET HAS A MATCHING CLOSING BRACKET
+    private boolean doBracketsMatch(String b) {
+        Stack<Character> allChar = new Stack<>();
+
+        for (int i = 0; i < b.length(); i++) {
+            allChar.push(b.charAt(i));
+        }
+
+        int allBrackets = 0;
+        while (!allChar.isEmpty()) {
+            Character aChar = allChar.pop();
+            if (aChar == '{')
+                allBrackets++;
+            else if (aChar == '}') {
+                allBrackets--;
+            }
+            if (allBrackets > 0)
+                return false;
+
+        }
+
+        return allBrackets == 0;
+
+    }
+
 }
